6676248bfa5db171b54913c4f6e15d4ef7cece82
TheManWhoLostTheWorld <denis.z.germany@gmail.com>
2023-12-03 18:06:05 +0100

    Add fixture Locators update

---
+++ tests/locators.py
@@ -0,0 +1,17 @@
+# Social Network Links
+TWITTER_BUTTON_HEADER = '//div[@class="col-6 col-sm-6"]/ul/li[1]'
+FACEBOOK_BUTTON_HEADER = '//div[@class="col-6 col-sm-6"]/ul/li[2]/a'
+YOUTUBE_BUTTON_HEADER = '//div[@class="col-6 col-sm-6"]/ul/li[3]/a'
+YOUTUBE_ACCEPT_BUTTON = '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span'
+INSTAGRAM_BUTTON_HEADER = '//div[@class="col-6 col-sm-6"]/ul/li[4]/a'
+TWITTER_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[1]/a'
+FACEBOOK_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[2]/a/i'
+YOUTUBE_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[3]/a/i'
+INSTAGRAM_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[4]/a/i'
+
+# Website
+MAIN_PAGE = "https://www.agoramodels.com/"
+TWITTER_PAGE = "https://twitter.com/AgoraModels"
+FACEBOOK_PAGE = "https://www.facebook.com/AgoraModelsOfficial/"
+YOUTUBE_PAGE = "https://www.youtube.com/AgoraModels"
+INSTAGRAM_PAGE = "https://www.instagram.com/agoramodels/"
--- tests/test_social_networks.py
+++ tests/test_social_networks.py
@@ -1,46 +1,47 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import time
+from locators import TWITTER_BUTTON_HEADER, MAIN_PAGE, TWITTER_PAGE, FACEBOOK_BUTTON_HEADER, FACEBOOK_PAGE, YOUTUBE_BUTTON_HEADER, YOUTUBE_ACCEPT_BUTTON, YOUTUBE_PAGE, INSTAGRAM_BUTTON_HEADER, INSTAGRAM_PAGE, TWITTER_BUTTON_FOOTER, FACEBOOK_BUTTON_FOOTER, YOUTUBE_BUTTON_FOOTER, INSTAGRAM_BUTTON_FOOTER
 
 driver = webdriver.Chrome()
 
 
-def test_twitter_link_header():
-    driver.get("https://www.agoramodels.com/")
+@pytest.fixture(autouse=True)
+def open_website():
+    return driver.get(MAIN_PAGE)
 
+
+def test_twitter_link_header():
     time.sleep(3)
 
-    twitter = driver.find_element(By.XPATH, '//div[@class="col-6 col-sm-6"]/ul/li[1]')
+    twitter = driver.find_element(By.XPATH, TWITTER_BUTTON_HEADER)
     twitter.click()
 
     time.sleep(3)
 
-    assert driver.current_url == "https://twitter.com/AgoraModels"
+    assert driver.current_url == TWITTER_PAGE
 
 
 def test_link_facebook_header():
-    driver.get("https://www.agoramodels.com/")
+    time.sleep(3)
+
+    facebook = driver.find_element(By.XPATH, FACEBOOK_BUTTON_HEADER).click()
 
     time.sleep(3)
 
-    facebook = driver.find_element(By.XPATH, '//div[@class="col-6 col-sm-6"]/ul/li[2]/a').click()
-
-    time.sleep(3)
-
-    assert driver.current_url == "https://www.facebook.com/AgoraModelsOfficial/"
+    assert driver.current_url == FACEBOOK_PAGE
 
 
 def test_link_youtube_header():
-    driver.get("https://www.agoramodels.com/")
+    time.sleep(3)
+
+    youtube = driver.find_element(By.XPATH, YOUTUBE_BUTTON_HEADER).click()
 
     time.sleep(3)
 
-    youtube = driver.find_element(By.XPATH, '//div[@class="col-6 col-sm-6"]/ul/li[3]/a').click()
-
-    time.sleep(3)
-
-    button_accept_cookies = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span')
+    button_accept_cookies = driver.find_element(By.XPATH, YOUTUBE_ACCEPT_BUTTON)
     ActionChains(driver)\
          .scroll_to_element(button_accept_cookies)\
          .perform()
@@ -49,43 +50,37 @@ def test_link_youtube_header():
 
     time.sleep(3)
 
-    assert driver.current_url == "https://www.youtube.com/AgoraModels"
+    assert driver.current_url == YOUTUBE_PAGE
 
 
 def test_link_instagram_header():
-    driver.get("https://www.agoramodels.com/")
+    time.sleep(3)
+
+    instagram = driver.find_element(By.XPATH, INSTAGRAM_BUTTON_HEADER).click()
 
     time.sleep(3)
 
-    instagram = driver.find_element(By.XPATH, '//div[@class="col-6 col-sm-6"]/ul/li[4]/a').click()
-
-    time.sleep(3)
-
-    assert driver.current_url == "https://www.instagram.com/agoramodels/"
+    assert driver.current_url == INSTAGRAM_PAGE
 
 
 def test_twitter_link_footer():
-    driver.get("https://www.agoramodels.com/")
-
     time.sleep(1)
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_twitter = driver.find_element(By.XPATH, '//div[@id="footer"]/footer/div/div/div[2]/li[1]/a')
+    footer_twitter = driver.find_element(By.XPATH, TWITTER_BUTTON_FOOTER)
     time.sleep(1) #now there is an issue. It should be solved by expected conditions
     footer_twitter.click()
 
     time.sleep(1)
 
-    assert driver.current_url == "https://twitter.com/AgoraModels"
+    assert driver.current_url == TWITTER_PAGE
 
 
 def test_facebook_link_footer():
-    driver.get("https://www.agoramodels.com/")
-
     time.sleep(1)
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_facebook = driver.find_element(By.XPATH, '//div[@id="footer"]/footer/div/div/div[2]/li[2]/a/i')
+    footer_facebook = driver.find_element(By.XPATH, FACEBOOK_BUTTON_FOOTER)
 
     time.sleep(1) #now there is an issue. It should be solved by expected conditions
 
@@ -93,43 +88,27 @@ def test_facebook_link_footer():
 
     time.sleep(3)
 
-    assert driver.current_url == "https://www.facebook.com/AgoraModelsOfficial/"
+    assert driver.current_url == FACEBOOK_PAGE
 
 
 def test_youtube_link_footer():
-    driver.get("https://www.agoramodels.com/")
-
     time.sleep(1)
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_youtube = driver.find_element(By.XPATH, '//div[@id="footer"]/footer/div/div/div[2]/li[3]/a/i')
+    footer_youtube = driver.find_element(By.XPATH, YOUTUBE_BUTTON_FOOTER)
     time.sleep(1) #now there is an issue. It should be solved by expected conditions
     footer_youtube.click()
 
     time.sleep(3)
 
-# there is an issue. Need to solve it
-
-    # button_accept_cookies = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/div[3]')
-    # ActionChains(driver)\
-    #      .scroll_to_element(button_accept_cookies)\
-    #      .perform()
-    #
-    # button_accept_cookies.click()
-    #
-    # time.sleep(3)
-    #SOLVED
-
-    assert driver.current_url == "https://www.youtube.com/AgoraModels"
+    assert driver.current_url == YOUTUBE_PAGE
 
 
 def test_instagram_link_footer():
-    driver.get("https://www.agoramodels.com/")
-
     time.sleep(1)
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_instagram = driver.find_element(By.XPATH, '//div[@id="footer"]/footer/div/div/div[2]/li[4]/a/i')
+    footer_instagram = driver.find_element(By.XPATH, INSTAGRAM_BUTTON_FOOTER)
 
     time.sleep(1) #now there is an issue. It should be solved by expected conditions
 
@@ -137,7 +116,7 @@ def test_instagram_link_footer():
 
     time.sleep(3)
 
-    assert driver.current_url == "https://www.instagram.com/agoramodels/"
+    assert driver.current_url == INSTAGRAM_PAGE
 
     driver.quit()
 
