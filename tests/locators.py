bc1e0584c32dadd423ea77b85f09eee4060e53ab
TheManWhoLostTheWorld <denis.z.germany@gmail.com>
2023-12-06 22:59:06 +0100

    Change time sleeps to explicity waits Add fixture for loading main page Add fixture for explicity waits

--- tests/locators.py
+++ tests/locators.py
@@ -8,10 +8,14 @@ TWITTER_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[1]/a'
 FACEBOOK_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[2]/a/i'
 YOUTUBE_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[3]/a/i'
 INSTAGRAM_BUTTON_FOOTER = '//div[@id="footer"]/footer/div/div/div[2]/li[4]/a/i'
+TWITTER_LOAD = "//div[@class='css-175oi2r r-1awozwy']"
+FACEBOOK_LOAD = '//div[@role="banner"]'
+YOUTUBE_LOAD = "//a[@id='logo']"
+INSTAGRAM_LOAD = "//div[@class='_aagx']"
 
 # Website
 MAIN_PAGE = "https://www.agoramodels.com/"
 TWITTER_PAGE = "https://twitter.com/AgoraModels"
-FACEBOOK_PAGE = "https://www.facebook.com/AgoraModelsOfficial/"
+FACEBOOK_PAGE = "https://www.facebook.com/AgoraModelsDE"
 YOUTUBE_PAGE = "https://www.youtube.com/AgoraModels"
 INSTAGRAM_PAGE = "https://www.instagram.com/agoramodels/"
--- tests/test_social_networks.py
+++ tests/test_social_networks.py
@@ -1,122 +1,142 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-from locators import TWITTER_BUTTON_HEADER, MAIN_PAGE, TWITTER_PAGE, FACEBOOK_BUTTON_HEADER, FACEBOOK_PAGE, YOUTUBE_BUTTON_HEADER, YOUTUBE_ACCEPT_BUTTON, YOUTUBE_PAGE, INSTAGRAM_BUTTON_HEADER, INSTAGRAM_PAGE, TWITTER_BUTTON_FOOTER, FACEBOOK_BUTTON_FOOTER, YOUTUBE_BUTTON_FOOTER, INSTAGRAM_BUTTON_FOOTER
-
-driver = webdriver.Chrome()
+from locators import TWITTER_BUTTON_HEADER, MAIN_PAGE, TWITTER_PAGE, FACEBOOK_BUTTON_HEADER, FACEBOOK_PAGE, YOUTUBE_BUTTON_HEADER, YOUTUBE_ACCEPT_BUTTON, YOUTUBE_PAGE, INSTAGRAM_BUTTON_HEADER, INSTAGRAM_PAGE, TWITTER_BUTTON_FOOTER, FACEBOOK_BUTTON_FOOTER, YOUTUBE_BUTTON_FOOTER, INSTAGRAM_BUTTON_FOOTER, TWITTER_LOAD, FACEBOOK_LOAD, YOUTUBE_LOAD, INSTAGRAM_LOAD
 
 
-@pytest.fixture(autouse=True)
-def open_website():
-    return driver.get(MAIN_PAGE)
+@pytest.fixture
+def chrome_options():
+    options = Options()
+    options.add_argument('--window-size=1920,1080')
+    return options
 
 
-def test_twitter_link_header():
-    time.sleep(3)
+@pytest.fixture
+def driver(chrome_options):
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver
+    driver.quit()
 
-    twitter = driver.find_element(By.XPATH, TWITTER_BUTTON_HEADER)
+
+@pytest.fixture
+def open_website(driver):
+    link = driver.get(MAIN_PAGE)
+    return link
+
+
+@pytest.fixture
+def wait(driver):
+    wait = WebDriverWait(driver, timeout=10)
+    return wait
+
+
+def test_twitter_link_header(driver, open_website, wait):
+
+    twitter = wait.until(EC.element_to_be_clickable((By.XPATH, TWITTER_BUTTON_HEADER)))
     twitter.click()
 
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, TWITTER_LOAD)))
 
     assert driver.current_url == TWITTER_PAGE
 
 
-def test_link_facebook_header():
-    time.sleep(3)
+def test_link_facebook_header(driver, open_website, wait):
 
-    facebook = driver.find_element(By.XPATH, FACEBOOK_BUTTON_HEADER).click()
+    facebook = wait.until(EC.element_to_be_clickable((By.XPATH, FACEBOOK_BUTTON_HEADER))).click()
 
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, FACEBOOK_LOAD)))
 
     assert driver.current_url == FACEBOOK_PAGE
 
 
-def test_link_youtube_header():
-    time.sleep(3)
+def test_link_youtube_header(driver, open_website, wait):
 
-    youtube = driver.find_element(By.XPATH, YOUTUBE_BUTTON_HEADER).click()
+    youtube = wait.until(EC.element_to_be_clickable((By.XPATH, YOUTUBE_BUTTON_HEADER))).click()
 
-    time.sleep(3)
-
-    button_accept_cookies = driver.find_element(By.XPATH, YOUTUBE_ACCEPT_BUTTON)
+    button_accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, YOUTUBE_ACCEPT_BUTTON)))
     ActionChains(driver)\
          .scroll_to_element(button_accept_cookies)\
          .perform()
 
     button_accept_cookies.click()
 
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, YOUTUBE_LOAD)))
 
     assert driver.current_url == YOUTUBE_PAGE
 
 
-def test_link_instagram_header():
-    time.sleep(3)
+def test_link_instagram_header(driver, open_website, wait):
 
-    instagram = driver.find_element(By.XPATH, INSTAGRAM_BUTTON_HEADER).click()
+    instagram = wait.until(EC.element_to_be_clickable((By.XPATH, INSTAGRAM_BUTTON_HEADER))).click()
 
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, INSTAGRAM_LOAD)))
 
     assert driver.current_url == INSTAGRAM_PAGE
 
 
-def test_twitter_link_footer():
-    time.sleep(1)
+def test_twitter_link_footer(driver, open_website, wait):
+    twitter = wait.until(EC.element_to_be_clickable((By.XPATH, TWITTER_BUTTON_HEADER)))
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
     footer_twitter = driver.find_element(By.XPATH, TWITTER_BUTTON_FOOTER)
-    time.sleep(1) #now there is an issue. It should be solved by expected conditions
+    time.sleep(1)
     footer_twitter.click()
 
-    time.sleep(1)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, TWITTER_LOAD)))
 
     assert driver.current_url == TWITTER_PAGE
 
 
-def test_facebook_link_footer():
-    time.sleep(1)
+def test_facebook_link_footer(driver, open_website, wait):
+
+    facebook = wait.until(EC.element_to_be_clickable((By.XPATH, FACEBOOK_BUTTON_HEADER)))
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_facebook = driver.find_element(By.XPATH, FACEBOOK_BUTTON_FOOTER)
+    time.sleep(1)
+    footer_facebook = driver.find_element(By.XPATH, FACEBOOK_BUTTON_FOOTER).click()
 
-    time.sleep(1) #now there is an issue. It should be solved by expected conditions
-
-    footer_facebook.click()
-
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, FACEBOOK_LOAD)))
 
     assert driver.current_url == FACEBOOK_PAGE
 
 
-def test_youtube_link_footer():
-    time.sleep(1)
+def test_youtube_link_footer(driver, open_website, wait):
+
+    youtube = wait.until(EC.element_to_be_clickable((By.XPATH, YOUTUBE_BUTTON_HEADER)))
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    footer_youtube = driver.find_element(By.XPATH, YOUTUBE_BUTTON_FOOTER)
-    time.sleep(1) #now there is an issue. It should be solved by expected conditions
-    footer_youtube.click()
+    time.sleep(1)
+    footer_youtube = driver.find_element(By.XPATH, YOUTUBE_BUTTON_FOOTER).click()
 
-    time.sleep(3)
+    button_accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, YOUTUBE_ACCEPT_BUTTON)))
+    ActionChains(driver) \
+        .scroll_to_element(button_accept_cookies) \
+        .perform()
+
+    button_accept_cookies.click()
+
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, YOUTUBE_LOAD)))
 
     assert driver.current_url == YOUTUBE_PAGE
 
 
-def test_instagram_link_footer():
-    time.sleep(1)
+def test_instagram_link_footer(driver, open_website, wait):
+    instagram = wait.until(EC.element_to_be_clickable((By.XPATH, INSTAGRAM_BUTTON_HEADER)))
 
     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
     footer_instagram = driver.find_element(By.XPATH, INSTAGRAM_BUTTON_FOOTER)
 
-    time.sleep(1) #now there is an issue. It should be solved by expected conditions
+    time.sleep(1)
 
     footer_instagram.click()
 
-    time.sleep(3)
+    load = wait.until(EC.presence_of_all_elements_located((By.XPATH, INSTAGRAM_LOAD)))
 
     assert driver.current_url == INSTAGRAM_PAGE
 
-    driver.quit()
 
